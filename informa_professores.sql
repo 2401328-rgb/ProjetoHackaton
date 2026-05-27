12f4936975080b8d38c76225b25d59a778f2552d
2101062luigi <2101062m@escolas.anchieta.br>
2026-05-27 12:10:14 -0300

    tabela banco de dados

---
+++ informa_professores.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27/05/2026 às 17:08
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `hackaton_grupo7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `informa_professores`
+--
+
+CREATE TABLE `informa_professores` (
+  `NOME` varchar(100) NOT NULL,
+  `SENHA` varchar(40) NOT NULL,
+  `EMAIL` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `informa_professores`
+--
+
+INSERT INTO `informa_professores` (`NOME`, `SENHA`, `EMAIL`) VALUES
+('sei la', '159', 'luigisanseca@gmail.com.br'),
+('sei la', '1', 'aaaaaaaaaaaaa@gmail.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
